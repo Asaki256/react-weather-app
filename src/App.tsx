e298ae9586200bfc75207672e47dd5e300386dac
Masaki <ryota423.jp@icloud.com>
2024-12-16 22:49:09 +0900

    React 基本 コンポーネント作成など

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import Title from './components/Title'
+import Form from './components/Form'
+
 const App = () => {
   return (
     <div>
-      <h1 className="test">こんにちは</h1>
-      <h3>さようなら</h3>
-      {false?(<p>True!</p>):(<p>False!</p>)}
+      <Title />
+      <Form />
     </div>
   )
 }
