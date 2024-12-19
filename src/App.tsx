8aecb5109d96bd6fff70028c91a1d1f5c38335b8
Masaki <ryota423.jp@icloud.com>
2024-12-19 23:44:40 +0900

    propsでデータ受け渡し

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
+import { useState } from 'react'
 import Title from './components/Title'
 import Form from './components/Form'
+import Results from './components/Results'
 
 const App = () => {
+  const [city, setCity] = useState<string>("");
+  const getWeather = (e: any) => {
+    e.preventDefault()
+    fetch("http://api.weatherapi.com/v1/current.json?key=fa849fbe1af6436ba7255502240903&q=London&aqi=no")
+      .then(res => res.json())
+      .then(data => console.log(data))
+  }
   return (
     <div>
       <Title />
-      <Form />
+      <Form setCity={setCity} getWeather={getWeather} />
+      <Results />
     </div>
   )
 }
--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,15 +1,11 @@
-import { useState } from 'react'
 
 
+type FormProps = {
+	setCity: React.Dispatch<React.SetStateAction<string>>
+	getWeather: (e: any) => void
+}
 
-const Form = () => {
-	const [city, setCity] = useState<string>("");
-	const getWeather = (e: any) => {
-		e.preventDefault()
-		fetch("http://api.weatherapi.com/v1/current.json?key=fa849fbe1af6436ba7255502240903&q=London&aqi=no")
-			.then(res => res.json())
-			.then(data => console.log(data))
-	}
+const Form = (props: FormProps) => {
 
 	return (
 		<form>
@@ -17,9 +13,9 @@ const Form = () => {
 				type="test"
 				name="city"
 				placeholder="都市名"
-				onChange={e => setCity(e.target.value)}
+				onChange={e => props.setCity(e.target.value)}
 			/>
-			<button type="submit" onClick={getWeather}>
+			<button type="submit" onClick={props.getWeather}>
 				Get Weather
 			</button>
 		</form>
